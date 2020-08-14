048e6b22f13fb7810dc4679b2e25efdc58a4763a
Dilan Perera <r.dilanperera@gmail.com>
2020-08-15 01:54:57 +0530

    Done

---
+++ set.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+b=0
+while [ $b -lt $[ ($RANDOM % 600 ) + 1 ] ]
+do
+	a=0
+	while [ $a -lt 10 ]
+	do
+		echo $[ ( $RANDOM % 100 )  + 1 ]  | tee -a file.txt
+		a=`expr $a + 1`
+	done
+	git add .
+	git commit -m "Done"
+	git push -u origin master
+	b=`expr $b + 1`
+done
