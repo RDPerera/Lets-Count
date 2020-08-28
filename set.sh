ef386eaa38b2e41aac312cf8c80c53d032a9ae4e
Dilan Perera <r.dilanperera@gmail.com>
2020-08-28 19:44:13 +0530

    Done

--- set.sh
+++ set.sh
@@ -5,7 +5,7 @@ do
 	a=0
 	while [ $a -lt 10 ]
 	do
-		echo $[ ( $RANDOM % 100 )  + 1 ]  | tee -a file.txt
+		echo $[ ( $RANDOM % 100 )  + 1 ]  | tee -a file.html
 		a=`expr $a + 1`
 	done
 	git add .
