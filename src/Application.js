4fda2b8128590109bafc2bfec08440df477063ce
Ruwanthi Hemachandra <56576410+RSH17@users.noreply.github.com>
2021-10-21 19:38:25 +0530

    Create Application.js

--- src/Application.js
+++ src/Application.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 import Counter from './Counter';
-// import CounterContainer from './CounterContainer';
-// import WithCount from './WithCount';
-
+/*import CounterContainer from './CounterContainer';
+import WithCount from './WithCount';
+*/
 export default class Application extends Component {
   render() {
     return (
