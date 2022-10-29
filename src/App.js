c1f6a0c4c00f1a3c76bced0a9b89a8262d80b404
Kalindhu_Navanjana <kalindhunavanjana@gmail.com>
2022-10-29 23:38:52 +0530

    React router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Welcome to React Router!</h1>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='about' element={<About />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
