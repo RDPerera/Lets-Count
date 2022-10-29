ae9f5762c6e09562055e9b81759ad2b631ed64a4
Kalindhu_Navanjana <kalindhunavanjana@gmail.com>
2022-10-30 02:28:41 +0530

    ABOUT AND HOME ROUTES

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home";
+import About from "./components/about";
 
 function App() {
     return (
         <div className='App'>
             <h1>Welcome to React Router!</h1>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Home/>} />
                 <Route path='about' element={<About />} />
             </Routes>
         </div>
